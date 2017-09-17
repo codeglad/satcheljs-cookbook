bb83150ee7c8963cf86d61b2484486196bf20fd9
Ken <kchau@microsoft.com>
2017-09-17 16:08:57 -0700

    adding a onclose clean up

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -9,10 +9,26 @@ const cx = classnames.bind(require('./AppStyles.css'));
 
 @observer
 export default class Description extends React.Component<any, any> {
+    constructor(props) {
+        super(props);
+        this.state = {hasError: false};
+    }
+
+    componentDidCatch(error, info) {
+        // Display fallback UI
+        this.setState({ hasError: true });
+        // You can also log the error to an error reporting service
+        console.log(error, info);
+    }
+
     render() {
         const store = getStore();
         const selectedBookId = getSelectedBookId();
         const book = selectedBookId !== null && store.books[selectedBookId];
+
+        if (this.state.hasError) {
+            return <div>ERROR!</div>;
+        }
 
         return (<div className={cx('description')}>
             {book && <h2>{book.name}</h2>}
--- src/orchestrators/popout.tsx
+++ src/orchestrators/popout.tsx
@@ -7,7 +7,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 orchestrator(showPopout, () => {
-    const openedWindow = window.open(undefined, 'yo', 'resizable,scrollbars,status');
-    openedWindow.document.write('<!DOCTYPE html><html><body><div id="child"></div></body></html>');
-    ReactDOM.render(<div><BookList /><Description /></div>, openedWindow.document.getElementById('child'));
+    let openedWindow = window.open(undefined, Math.random().toString(12).slice(2), 'resizable,scrollbars,status');
+    (window as any).childOnUnload = function(ReactDOMInstance) {
+        ReactDOM.unmountComponentAtNode((this as Window).document.getElementById('child') as any);
+    };
+    
+    openedWindow.document.write('<!DOCTYPE html><html><body><div id="child"></div><script>window.onbeforeunload=window.opener.childOnUnload</script></body></html>');
+    
+    let child = openedWindow.document.getElementById('child');    
+    ReactDOM.render(<div><BookList /><Description /></div>, child!);
 });
